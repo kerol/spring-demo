77f312c1377c6158984e91a09370149d789e1b94
kerol <ikerol@163.com>
2016-12-22 13:21:52 +0800

    update

--- src/main/java/me/kerol/Application.java
+++ src/main/java/me/kerol/Application.java
@@ -1,4 +1,4 @@
-package me.kerol.hello;
+package me.kerol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/me/kerol/World.java
+++
@@ -1,9 +0,0 @@
-package me.kerol;
-import java.util.Date;
-
-public class World{
-    public static final String str = "world!";
-    public void print_world() {
-        System.out.println(this.str);
-    }
-}
